e7aacd9fd8e36a5c3e955ba6507f9eca4e9d1080
ImamHaris <38088979+ImamHaris@users.noreply.github.com>
2022-07-17 06:10:20 +0700

    widget gender

--- lib/bmi_data_screen.dart
+++ lib/bmi_data_screen.dart
@@ -14,6 +14,7 @@ class _BmiDataScreenState extends State<BmiDataScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: Color(0xff0A0E21),
       appBar: AppBar(
         title: Text("BMI Calculator"),
       ),
@@ -25,8 +26,10 @@ class _BmiDataScreenState extends State<BmiDataScreen> {
                 Expanded(
                   child: Container(
                     decoration: BoxDecoration(
-                        color: Colors.grey,
-                        borderRadius: BorderRadius.circular(15)),
+                      color: Color(0xff272A4B),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    margin: EdgeInsets.all(15),
                     child: Column(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: const [
@@ -46,21 +49,28 @@ class _BmiDataScreenState extends State<BmiDataScreen> {
                   ),
                 ),
                 Expanded(
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: const [
-                      Icon(
-                        Icons.female,
-                        size: 80,
-                      ),
-                      Text(
-                        "Wanita",
-                        style: TextStyle(
-                          fontSize: 18,
-                          color: Color(0xff8d8e98),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Color(0xff272A4B),
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                    margin: EdgeInsets.all(15),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: const [
+                        Icon(
+                          Icons.female,
+                          size: 80,
                         ),
-                      ),
-                    ],
+                        Text(
+                          "Wanita",
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Color(0xff8d8e98),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 )
               ],
